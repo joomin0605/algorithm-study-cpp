2c6ca585743b3887130fd912812b335b6f7533fa
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-15 16:33:50 +0900

    [Passed] 소수점 반올림하기 (8ms, 0MB)

---
+++ trail1/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a = 25.352;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << a;
+    return 0;
+}
