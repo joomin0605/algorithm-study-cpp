500f6c16375fb1644f71619a67d274e5eb5f04f8
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 15:48:26 +0900

    [Passed] 길이 단위 환산하기 (8ms, 0MB)

---
+++ trail1/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float ft = 0;
+
+    cin >> ft;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << ft * 30.48;
+    return 0;
+}
