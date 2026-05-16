fe30e7c4fac63c0aa4255e859ac011ef5c72afe8
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 16:16:34 +0900

    [Passed] 몫과 나머지 (8ms, 0MB)

---
+++ trail1/몫과 나머지/quotient-remainder.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b = 0;
+
+    cin >> a >> b;
+    cout << a / b << "..." << a % b;
+    return 0;
+}
