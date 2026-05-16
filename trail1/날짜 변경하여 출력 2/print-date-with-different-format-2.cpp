80e751be2442497c4164bbca53be227dc836811e
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 16:06:16 +0900

    [Passed] 날짜 변경하여 출력 2 (8ms, 0MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c = 0;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << c << "." << a << "." << b;
+    return 0;
+}
