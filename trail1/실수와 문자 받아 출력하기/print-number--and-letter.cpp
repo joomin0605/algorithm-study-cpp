811a1d3a155650781fc7d5cd0eb55e04bb851923
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 16:00:34 +0900

    [Passed] 실수와 문자 받아 출력하기 (8ms, 0MB)

---
+++ trail1/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    double a, b = 0;
+
+    cin >> c >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << c << endl << a << endl << b;
+    return 0;
+}
