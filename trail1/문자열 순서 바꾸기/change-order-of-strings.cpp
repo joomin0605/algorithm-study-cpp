e79438d9f1f67c44d262385c53fc6afda8f79e0a
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 16:01:23 +0900

    [Passed] 문자열 순서 바꾸기 (8ms, 0MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a, b;
+
+    cin >> a >> b;
+
+    cout << b << endl << a;
+    return 0;
+}
