ed910defbaeb5bea81cdd46e875cacb76cde3c3a
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 16:07:00 +0900

    [Passed] 입력받아 출력 2 (9ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b = 0;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+
+    cout << a << b;
+    return 0;
+}
