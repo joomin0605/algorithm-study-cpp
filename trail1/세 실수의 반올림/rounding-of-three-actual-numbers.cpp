7fae6a1012feff0a2c2be2b7665320c201ee313a
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 15:56:37 +0900

    [Passed] 세 실수의 반올림 (9ms, 0MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c = 0;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << endl << b << endl << c;
+    return 0;
+}
