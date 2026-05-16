0a26e22a798bddeea87df0dacd0ff9aae955f00c
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 16:10:38 +0900

    [Passed] 전화번호 바꾸기 (8ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c = 0;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << "010-" << c << "-" << b;
+    return 0;
+}
