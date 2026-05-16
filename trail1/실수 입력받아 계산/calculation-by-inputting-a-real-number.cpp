d8094538a8ca1937bc653f4be5345b309ce9f0b4
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-16 15:55:19 +0900

    [Passed] 실수 입력받아 계산 (8ms, 0MB)

---
+++ trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a, b = 0;
+
+    cin >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a + b;
+    return 0;
+}
