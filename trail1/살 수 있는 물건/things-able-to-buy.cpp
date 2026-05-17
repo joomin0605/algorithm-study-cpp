15d4a6d386cfd59f5b613590c3d7ef1787c76949
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:23:13 +0900

    [Passed] 살 수 있는 물건 (8ms, 0MB)

---
+++ trail1/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+
+    if(n >= 3000) {
+        cout << "book";
+    }
+    else if(n >= 1000) {
+        cout << "mask";
+    }
+    else {
+        cout << "no";
+    }
+    return 0;
+}
