6fb3b711ee2032ff3220b6d1f01e07fd4ae84502
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:35:40 +0900

    [Passed] 두 수의 짝홀 여부 (8ms, 0MB)

---
+++ trail1/두 수의 짝홀 여부/parity-of-two-numbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b = 0;
+
+    cin >> a >> b;
+
+    if(a % 2 == 0) {
+        cout << "even";
+    }
+    else {
+        cout << "odd";
+    }
+
+    cout << "\n";
+
+    if(b % 2 == 0) {
+        cout << "even";
+    }
+    else {
+        cout << "odd";
+    }
+    return 0;
+}
