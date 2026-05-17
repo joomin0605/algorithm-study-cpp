3e1267a514a73845b34f6089ccffa5d0cdb5a99c
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:21:58 +0900

    [Passed] 시력 검사 2 (8ms, 0MB)

---
+++ trail1/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a = 0;
+
+    cin >> a;
+
+    if(a >= 1.0) {
+        cout << "High";
+    }
+    else if(a >= 0.5) {
+        cout << "Middle";
+    }
+    else {
+        cout << "Low";
+    }
+    return 0;
+}
