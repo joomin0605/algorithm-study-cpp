bf5caabb2eb0cd8ef90c38051c34d01bfc2af229
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:17:38 +0900

    [Passed] 삼항연산자 2 (8ms, 0MB)

---
+++ trail1/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    char c;
+
+    cin >> a;
+
+    c = a == 1 ? 't' : 'f';
+
+    cout << c;
+    return 0;
+}
