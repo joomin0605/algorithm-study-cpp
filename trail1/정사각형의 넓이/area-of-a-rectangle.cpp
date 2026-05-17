4956a1d7ce6d93febe154e3d38976d2262f2e147
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:09:00 +0900

    [Passed] 정사각형의 넓이 (8ms, 0MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+
+    if(n < 5) {
+        cout << n * n << endl << "tiny";
+    }
+    else {
+        cout << n * n;
+    }
+    return 0;
+}
