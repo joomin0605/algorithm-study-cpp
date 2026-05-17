2546fac9673edaeb772f72c81a794ad6cee9e81c
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:13:58 +0900

    [Passed] 2개 중 최대 (8ms, 0MB)

---
+++ trail1/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b = 0;
+    int c = 0;
+
+    cin >> a >> b;
+
+    c = a > b ? a : b;
+    cout << c;
+    return 0;
+}
