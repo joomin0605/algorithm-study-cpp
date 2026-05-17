6a0fc398d61776cb589ea2d52dc2c02ed90499aa
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:33:07 +0900

    [Passed] 정수의 조건 여부 2 (8ms, 0MB)

---
+++ trail1/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+
+    cin >> a;
+
+    if(a == 5) {
+        cout << "A";
+    }
+    if(a % 2 == 0) {
+        cout << "B";
+    }
+    return 0;
+}
