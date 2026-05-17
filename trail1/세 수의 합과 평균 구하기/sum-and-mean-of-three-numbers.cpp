f55c27b15d6267126c85980bc19e0ca256a9a7dc
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 11:59:27 +0900

    [Passed] 세 수의 합과 평균 구하기 (8ms, 0MB)

---
+++ trail1/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c = 0;
+
+    cin >> a >> b >> c;
+    
+    cout << a + b + c << endl << (a + b + c) / 3;
+    return 0;
+}
