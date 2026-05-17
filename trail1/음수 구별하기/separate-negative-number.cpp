1abba3047ba8427acebb3ffd0a07ba40127c9e54
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-17 12:03:10 +0900

    [Passed] 음수 구별하기 (8ms, 0MB)

---
+++ trail1/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+
+    cin >> a;
+
+    cout << a << endl;
+    if(a < 0) {
+        cout << "minus";
+    }
+    return 0;
+}
