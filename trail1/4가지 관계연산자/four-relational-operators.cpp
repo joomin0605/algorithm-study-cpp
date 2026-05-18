783d310971ab28418192e5acecab288e597d9f5c
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-18 12:59:00 +0900

    [Passed] 4가지 관계연산자 (8ms, 0MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b = 0;
+
+    cin >> a >> b;
+
+    cout << (a >= b) << endl;
+    cout << (a > b) << endl;
+    cout << (b >= a) << endl;
+    cout << (b > a);
+    return 0;
+}
