48e22b4f1b8d21e7369874342a8c9cb10f1fbf22
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-18 13:24:05 +0900

    [Passed] 굉장한 수 (8ms, 0MB)

---
+++ trail1/굉장한 수/amazing-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+
+    if((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)) {
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+    return 0;
+}
