7079d2c61c7fc22133cf8a43a28a451cb095e608
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-18 17:06:43 +0900

    [Passed] 일 수 구하기 (8ms, 0MB)

---
+++ trail1/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+
+    if((n == 4 || n == 6 || n == 9 || n == 11) && n != 2) {
+        cout << 30;
+    }
+    else if(n == 2) {
+        cout << 28;
+    }
+    else {
+        cout << 31;
+    }
+    return 0;
+}
