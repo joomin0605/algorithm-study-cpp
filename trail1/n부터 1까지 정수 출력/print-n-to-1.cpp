27ddd3f53b35fd0f5bd25ec5e9b5a5fc548db797
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-19 17:38:50 +0900

    [Passed] n부터 1까지 정수 출력 (2ms, 0MB)

---
+++ trail1/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+
+    while(n >= 1) {
+        cout << n << " ";
+        n--;
+    }
+    return 0;
+}
