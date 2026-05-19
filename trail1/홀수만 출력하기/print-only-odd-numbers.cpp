fd8ed411b2310bdf598fefb77f834b026d0e552c
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-20 02:15:18 +0900

    [Passed] 홀수만 출력하기 (2ms, 0MB)

---
+++ trail1/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+    int a = 0;
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> a;
+
+        if((a % 2 != 0) && (a % 3 == 0)) {
+            cout << a << endl;
+        }
+    }
+    return 0;
+}
