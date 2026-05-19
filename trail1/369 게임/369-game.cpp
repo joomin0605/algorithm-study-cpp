2de7f8ddf4495f5590c661502b0d2d62b74d87c0
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-20 02:11:19 +0900

    [Passed] 369 게임 (1ms, 0MB)

---
+++ trail1/369 게임/369-game.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        if(i % 3 == 0) {
+            cout << 0 << " ";
+        }
+        else if((i / 10 == 3) || (i % 10 == 3)) {
+            cout << 0 << " ";
+        }
+        else if((i / 10 == 6) || (i % 10 == 6)) {
+            cout << 0 << " ";
+        }
+        else if((i / 10 == 9) || (i % 10 == 9)) {
+            cout << 0 << " ";
+        }
+        else {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
