83dca093cb31d193215956513b3975407bacf1fe
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-19 17:23:10 +0900

    [Passed] 홀수만 출력 2 (3ms, 0MB)

---
+++ trail1/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int b, a = 0;
+
+    cin >> b >> a;
+
+    for(int i = b; i >= a; i -= 2) {
+        cout << i << " ";
+    }
+    return 0;
+}
