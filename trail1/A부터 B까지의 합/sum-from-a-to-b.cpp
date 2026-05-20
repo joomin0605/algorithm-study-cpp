f6317b25a772b48256fad60bf60c1ed68847f7b6
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-20 14:51:52 +0900

    [Passed] A부터 B까지의 합 (2ms, 0MB)

---
+++ trail1/A부터 B까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0, b = 0;
+    int sum = 0;
+
+    cin >> a >> b;
+
+    for(int i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    cout << sum;
+    return 0;
+}
