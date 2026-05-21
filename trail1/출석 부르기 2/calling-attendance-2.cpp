eec418832c6524b38b29b70417beffa669bdde5b
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-21 23:02:10 +0900

    [Passed] 출석 부르기 2 (2ms, 0MB)

---
+++ trail1/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    while(1) {
+        cin >> n;
+
+        if(n == 1) {
+            cout << "John" << endl;
+        }
+        else if(n == 2) {
+            cout << "Tom" << endl;
+        }
+        else if(n == 3) {
+            cout << "Paul" << endl;
+        }
+        else if(n == 4) {
+            cout << "Sam" << endl;
+        }
+        else {
+            cout << "Vacancy";
+            break;
+        }
+    }
+    return 0;
+}
