a95da3e7501a062dbe215874d2ae2c36596f014d
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-22 18:51:40 +0900

    [Passed] 별 그리기 (2ms, 0MB)

---
+++ trail1/별 그리기/star-drawing.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n - 1; j++) {
+            cout << " ";
+        }
+        for(int j = 0; j < 2 * i + 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < i + 1; j++) {
+            cout << " ";
+        }
+        for(int j = 0; j < (2 * n) - (2 * i) - 3; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
