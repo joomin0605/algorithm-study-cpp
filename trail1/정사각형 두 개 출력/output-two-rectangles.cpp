1e11e09519c8a152a511b4f4e779403c36465f3b
Han JuMin <79889924+joomin0605@users.noreply.github.com>
2026-05-22 15:48:07 +0900

    [Passed] 정사각형 두 개 출력 (2ms, 0MB)

---
+++ trail1/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+
+    cin >> n;
+
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < n; k++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
